4234902666400300c30165014d670a33dd1e43e8
debbie <drosenfeld87@gmail.com>
2018-03-25 12:37:04 -0400

    wip sword functions

--- js/scene2.js
+++ js/scene2.js
@@ -40,51 +40,50 @@ function scene2() {
 
   // Character 2
   var joffreyContainer = document.createElement('div');
+  joffreyContainer.id = 'joffrey-container'
   stage.append(joffreyContainer)
 
   joffreyContainer.style.position = 'absolute';
-  joffreyContainer.style.width = '10%';
-  joffreyContainer.style.height = '44%';
+  joffreyContainer.style.width = '8%';
+  joffreyContainer.style.height = '31%';
   joffreyContainer.style.top = '55%';
-  joffreyContainer.style.left = '10%';
-  joffreyContainer.style.border = '2px solid blue';
-  joffreyContainer.style.background = 'yellow';
+  joffreyContainer.style.left = '55%';
 
   var joffrey = document.querySelector('#joffrey');
   joffreyContainer.append(joffrey);
-  var king = document.querySelector('#king');
-  joffreyContainer.append(king);
-
-  joffrey.style.width = '70%;';
+  joffrey.style.width = '75%';
   joffrey.style.position = 'absolute';
-  joffrey.style.top = 0;
-  joffrey.style.position = 'absolute';
-  joffrey.style.border = '2px solid green';
-  joffrey.style.bottom = 0;
+  joffrey.style.left = "33%";
 
-  joffrey.style.border = 'none';
-  leather.style.border = 'none';
-  joffreyContainer.style.border = 'none';
-  joffreyContainer.style.background = 'none';
+
+  kingWardrobe = document.querySelector('#king');
+  joffreyContainer.append(kingWardrobe);
+  kingWardrobe.style.position = 'absolute';
+  kingWardrobe.style.bottom = 0;
 
   // Setup Props
 
   // Prop 1
-
+  sword = document.querySelector('#sword');
+  stage.append(sword);
+  sword.style.position = 'absolute';
+  sword.style.top = '77%';
+  sword.style.left = '31%';
+  sword.style.height = '5%';
+  sword.style.transform = 'rotateY(150deg)';
   // Prop 2
+
 
   // Action!
 
   // Stage direction 1
 
   frame(function() {
-
+    sword.style.top = '77%'
+    sword.style.left = '31%'
   })
-
-  // Stage direction 2
 
   frame(function() {
-
+    sword.style.top = '64%'
+    sword.style.left = '53%'
   })
-
-}
